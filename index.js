886b772257d6c3a5307bb4a2ce9540699a9d8a01
utkamag <92789705+utkamag@users.noreply.github.com>
2022-09-26 21:46:01 +0400

    update code

--- index.js
+++ index.js
@@ -4,5 +4,23 @@ const PORT = 5000;
 
 const app = express()
 
-app.listen(PORT, () => console.log("Server Started ON Port" + PORT ))
+//Firebase
+
+rules_version = '2';
+service cloud.firestore {
+    match /databases/{database}/documents {
+    match /{document=**} {
+        allow read, write: if false;
+    }
+}
+}
+
+app.use(express.json())
+
+app.post("/", (req, res) => {
+    console.log(req.body)
+    res.status(200).json("Сервер работает!")
+})
+
+app.listen(PORT, () => console.log("Server Started ON Port" + PORT))
 
