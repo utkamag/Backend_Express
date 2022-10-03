e30b3841804a3468efe66a40bcbea6428f360e81
utkamag <92789705+utkamag@users.noreply.github.com>
2022-10-03 18:49:22 +0400

    update code

--- router.js
+++ router.js
@@ -9,6 +9,6 @@ router.post('/post', PostController.create)
 router.get('/post', PostController.getAll)
 router.get('/post/:id', PostController.getOne)
 router.put('/post', PostController.update)
-router.delete('/post:id', PostController.delete)
+router.delete('/post/:id', PostController.delete)
 
 export default router;
