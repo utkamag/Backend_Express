5203b7039d4474587796e937e554b6210bc0c6c3
utkamag <92789705+utkamag@users.noreply.github.com>
2022-10-04 13:39:15 +0400

    update code

--- router.js
+++ router.js
@@ -5,7 +5,6 @@ import PostController from "./PostController.js";
 const router = new Router()
 
 router.post('/post', PostController.create)
-
 router.get('/post', PostController.getAll)
 router.get('/post/:id', PostController.getOne)
 router.put('/post', PostController.update)
